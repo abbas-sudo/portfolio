8f69b67a4c341610561e468bfe06b04c7ce5a736
Abbas <abbasialimohd@gmail.com>
2021-03-16 01:27:31 +0530

    Adding Dynamic Carts

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,53 +1,63 @@
 const project = [
     {
         id : 1,
-        img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work1.jpg",
-        title : "Mohammad Abbas",
-        cardsub : "Someonssse name",
-        descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        img : "../assets/img/portfolio.png",
+        title : "Portfolio",
+        cardsub : "HTML + CSS + JS + BOOTSTRAP",
+        descrpt : "I created this website so I could showcase all my projects and education.",
+        github : "https://github.com/abbas-sudo/portfolio",
+        launch: "https://abbas-sudo.github.io/portfolio/"
     },
     {
         id : 2,
-        img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work2.jpg",
-        title : "Someonssse name",
-        cardsub : "Someonssse name",
-        descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        img : "../assets/img/recipe.png",
+        title : "Recipe Guide",
+        cardsub : "React Js + API",
+        descrpt : "A website build on react js with Recipe API for serving the Recipes according to the user search.",
+        github : "https://github.com/abbas-sudo/React-API-Recipe",
+        launch: "https://abbas-sudo.github.io/React-API-Recipe/"
     },
     {
         id : 3,
-        img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work3.jpg",
-        title : "Someonssse name",
-        cardsub : "Someonssse name",
-        descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        img : "../assets/img/covid.png",
+        title : "Corona Tracker",
+        cardsub : "React Js + API + Bootstrap5",
+        descrpt : "Corona Tracker thats show the current stat's of total corana cases by countries and world.",
+        github : "https://github.com/abbas-sudo/corona-tracker",
+        launch: "https://abbas-sudo.github.io/corona-tracker/"
     },
     {
         id : 4,
-        img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work4.jpg",
-        title : "Someonssse name",
-        cardsub : "Someonssse name",
-        descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        id : 5,
-        img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work5.jpg",
-        title : "Someonssse name",
-        cardsub : "Someonssse name",
-        descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-    },
-    
-    {
-        id : 6,
-        img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work6.jpg",
-        title : "Someonssse name",
-        cardsub : "Someonssse name",
-        descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        img : "../assets/img/food.png",
+        title : "Restaurant E-Commerce",
+        cardsub : "HTML + SASS",
+        descrpt : "A E-commerce website build on pure SASS to showcase my CSS preprosessor Skills. ",
+        github : "https://github.com/abbas-sudo/restaurants-SASS",
+        launch: "https://abbas-sudo.github.io/restaurants-SASS/"
     }
+    // {
+    //     id : 5,
+    //     img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work5.jpg",
+    //     title : "Someonssse name",
+    //     cardsub : "Someonssse name",
+    //     descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    // },
+    
+    // {
+    //     id : 6,
+    //     img : "https://github.com/abbas-sudo/portfolio/raw/master/assets/img/work6.jpg",
+    //     title : "Someonssse name",
+    //     cardsub : "Someonssse name",
+    //     descrpt : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    // }
 ];
 
 const img = document.getElementById("cardimg");
 const title = document.getElementById("cardtitle");
 const cardsub = document.getElementById("card-subtitle");
 const descrpt = document.getElementById("descrpt");
+const github = document.getElementById("github");
+const launch = document.getElementById("launch");
 
 const box = document.querySelector(".box");
 
@@ -60,8 +70,8 @@ window.addEventListener("DOMContentLoaded", function () {
           <h5 class="card-title" id="cardtitle">${item.title}</h5>
           <h6 class="card-subtitle mb-2 text-muted" id="card-subtitle">${item.cardsub}</h6>
           <p class="card-text" id="descrpt">${item.descrpt}</p>
-          <a href="#" class="btn btn-outline-primary w-50"><i class="fa fa-github-square" aria-hidden="true"> Github</i></a>
-        <a href="#" class="btn btn-primary "><i class="fa fa-location-arrow" aria-hidden="true"> Launch</i></a>
+          <a href="${item.github}" target= "_blank" class="btn btn-outline-primary w-50 " id="github"><i class="fa fa-github-square" aria-hidden="true">Github</i></a>
+        <a href="${item.launch}" target= "_blank" class="btn btn-primary " id="launch"><i class="fa fa-location-arrow" aria-hidden="true">Launch</i></a>
               </div>
                       </div>
                       </div>`;
